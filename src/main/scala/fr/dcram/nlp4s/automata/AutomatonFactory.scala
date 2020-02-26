1fe2b1ada4956b2bb0e72b9c37a95c471a23f600
Damien Cram <dcr@sparklane.fr>
2020-02-26 10:52:18 +0100

    Tested AutomatonFactory

--- src/main/scala/fr/dcram/nlp4s/automata/AutomatonFactory.scala
+++ src/main/scala/fr/dcram/nlp4s/automata/AutomatonFactory.scala
@@ -1,6 +1,7 @@
 package fr.dcram.nlp4s.automata
 
 object AutomatonFactory {
+  import Quantifiers._
   def sequence[Tok](transitionables:Transitionable[Tok]*):Automaton[Tok] = {
     val initState = transitionables.foldRight(newAcceptingState[Tok]){
       case (t, target) => State(transitions = Seq(t.asTransition(target)))
@@ -17,15 +18,18 @@ object AutomatonFactory {
 
   def named[Tok](name:String, a:Automaton[Tok]):Automaton[Tok] = Automaton(State(transitions = Seq(AutTransit(Some(name), a, newAcceptingState))))
 
-  def quantified[Tok](t:Transitionable[Tok], quantifier:Quantifiers.Quantifier):Automaton[Tok] = quantifier match {
-    case Quantifiers.ZeroOne => zeroOne(t)
-    case Quantifiers.MN(m,n) if m == n => quantified(t, Quantifiers.N(m))
-    case Quantifiers.MN(m,n) => sequence(quantified(t, Quantifiers.N(m)),quantified(t, Quantifiers.ZeroN(m-n)))
-    case Quantifiers.ZeroN(n) => sequence(Seq.fill(n)(quantified(t, Quantifiers.ZeroOne)):_*)
-    case Quantifiers.NStar(n) => sequence(quantified(t, Quantifiers.N(n)), quantified(t, Quantifiers.Star))
-    case Quantifiers.N(n) => sequence(Seq.fill(n)(t):_*)
-    case Quantifiers.Star => star(t)
-    case Quantifiers.Plus => sequence(t, quantified(t, Quantifiers.Star))
+  def quantified[Tok](t:Transitionable[Tok], quantifier:Quantifier):Automaton[Tok] = quantifier match {
+    case ZeroOne => zeroOne(t)
+    case MN(m,n) if m == n => quantified(t, N(m))
+    case MN(m,n) => sequence(quantified(t, N(m)),quantified(t, ZeroN(n-m)))
+    case ZeroN(1) => zeroOne(t)
+    case ZeroN(n) => sequence(Seq.fill(n)(quantified(t, ZeroOne)):_*)
+    case NStar(0) => quantified(t, Star)
+    case NStar(1) => quantified(t, Plus)
+    case NStar(n) => sequence(quantified(t, N(n)), quantified(t, Star))
+    case N(n) => sequence(Seq.fill(n)(t):_*)
+    case Star => star(t)
+    case Plus => sequence(t, quantified(t, Star))
     case e => throw new UnsupportedOperationException(s"Unsupported quantifier: ${e.getClass.getSimpleName}")
   }
 
@@ -36,8 +40,8 @@ object AutomatonFactory {
 
   def star[Tok](t:Transitionable[Tok]):Automaton[Tok] = {
     new AutomatonBuilder().initialState(1).state(2, accepting = true)
+      .transition(1, 1, t)
       .epsilon(1, 2)
-      .transition(1, 2, t)
       .build
   }
 
--- src/main/scala/fr/dcram/nlp4s/automata/Quantifiers.scala
+++ src/main/scala/fr/dcram/nlp4s/automata/Quantifiers.scala
@@ -5,10 +5,15 @@ object Quantifiers {
   object Star extends Quantifier                  // *
   object Plus extends Quantifier                  // +
   object ZeroOne extends Quantifier               // ?
-  case class N(n:Int) extends Quantifier          // {n}
+  case class N(n:Int) extends Quantifier {        // {n}
+    require(n > 0, s"Bad {n} quantifier. n > 0 required. Got: {$n}")
+  }
   case class MN(m:Int, n:Int) extends Quantifier {// {m,n}
-    require(m>=n, s"Bad {m,n} quatifier. m>=n required. Got: {$m,$n}")
+    require(m<=n, s"Bad {m,n} quantifier. m<=n required. Got: {$m,$n}")
   }
   case class NStar(n:Int) extends Quantifier      // {n,}
-  case class ZeroN(n:Int) extends Quantifier      // {,n}
+  case class ZeroN(n:Int) extends Quantifier {      // {,n}
+    require(n > 0, s"Bad {,n} quantifier. n > 0 required. Got: {,$n}")
+
+  }
 }

ec6a229cae47a2bef307a360153df01bb373fdff
Damien Cram <dcr@sparklane.fr>
2020-05-07 13:39:21 +0200

    Added #filter and #filterS to tokenizers

--- src/main/scala/fr/dcram/nlp4s/ner/Tokenizers.scala
+++ src/main/scala/fr/dcram/nlp4s/ner/Tokenizers.scala
@@ -10,9 +10,14 @@ trait Tokenizers extends Monoid[StringTokenizer] {
       case Token(begin2, end2, str) => Token(tok.begin + begin2, tok.begin + end2, str)
     })
 
+  def filter(t: StringTokenizer)(f:Token[String] => Boolean):StringTokenizer = str => t(str).filter(f)
+  def filterS(t: StringTokenizer)(f:String => Boolean):StringTokenizer = str => t(str).filter(t => f(t.obj))
+
   implicit def tokenizerOps(t:StringTokenizer):TokenizerOps = TokenizerOps(t)
 
   case class TokenizerOps(t:StringTokenizer) {
     def chain(t2:StringTokenizer):StringTokenizer = self.op(t, t2)
+    def filter(f:Token[String] => Boolean):StringTokenizer = self.filter(t)(f)
+    def filterS(f:String => Boolean):StringTokenizer = self.filterS(t)(f)
   }
 }
